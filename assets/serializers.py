1d816e038546d3837b9e438b113cea744b1b936c
Abraham Martin <abraham.martin@gmail.com>
2018-01-22 19:05:33 +0000

    added initial tests

--- assets/serializers.py
+++ assets/serializers.py
@@ -3,14 +3,18 @@ from assets.models import Asset
 
 
 class AssetSerializer(serializers.HyperlinkedModelSerializer):
-    data_subject = fields.MultipleChoiceField(choices=Asset.DATA_SUBJECT_CHOICES)
-    data_category = fields.MultipleChoiceField(choices=Asset.DATA_CATEGORY_CHOICES)
-    risk_type = fields.MultipleChoiceField(choices=Asset.RISK_CHOICES)
-    storage_format = fields.MultipleChoiceField(choices=Asset.STORAGE_FORMAT_CHOICES)
+    data_subject = fields.MultipleChoiceField(choices=Asset.DATA_SUBJECT_CHOICES,
+                                              allow_null=True, allow_blank=True)
+    data_category = fields.MultipleChoiceField(choices=Asset.DATA_CATEGORY_CHOICES,
+                                               allow_null=True, allow_blank=True)
+    risk_type = fields.MultipleChoiceField(choices=Asset.RISK_CHOICES, allow_null=True,
+                                           allow_blank=True)
+    storage_format = fields.MultipleChoiceField(choices=Asset.STORAGE_FORMAT_CHOICES,
+                                                allow_null=True, allow_blank=True)
     paper_storage_security = fields.MultipleChoiceField(
-        choices=Asset.PAPER_STORAGE_SECURITY_CHOICES)
+        choices=Asset.PAPER_STORAGE_SECURITY_CHOICES, allow_null=True, allow_blank=True)
     digital_storage_security = fields.MultipleChoiceField(
-        choices=Asset.DIGITAL_STORAGE_SECURITY_CHOICES)
+        choices=Asset.DIGITAL_STORAGE_SECURITY_CHOICES, allow_null=True, allow_blank=True)
     class Meta:
         model = Asset
         fields = '__all__'
--- assets/tests.py
+++ assets/tests.py
@@ -1,3 +1,48 @@
+from automationcommon.tests.utils import UnitTestCase
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class APIViewsTests(UnitTestCase):
+
+    def test_asset_post(self):
+        client = APIClient()
+        asset_json = {
+            "data_subject": [
+                "students"
+            ],
+            "data_category": [
+                "personal",
+                "research"
+            ],
+            "risk_type": [
+                "operational",
+                "reputational"
+            ],
+            "storage_format": [
+                "digital"
+            ],
+            "paper_storage_security": [
+                "locked_cabinet"
+            ],
+            "digital_storage_security": [
+                "acl"
+            ],
+            "name": "asset1",
+            "department": "department test",
+            "purpose": "don't know",
+            "owner": "amc203",
+            "private": True,
+            "personal_data": False,
+            "data_category_others": "",
+            "recipients_category": "no idea",
+            "recipients_outside_eea": False,
+            "recipients_outside_eea_who": "",
+            "retention": "<=1",
+            "retention_other": "",
+            "storage_location": "Who knows"
+        }
+
+        result_post = client.post('/assets/', asset_json, format='json')
+        result_get = client.get(result_post['url'], format='json')
+        self.assertEqual(asset_json, result_get)
