986290b51c7be48d39be267654f1867ed6e23037
Abraham Martin <amc203@cam.ac.uk>
2018-01-23 14:19:14 +0000

    add more tests

--- assets/tests.py
+++ assets/tests.py
@@ -55,3 +55,55 @@ class APIViewsTests(UnitTestCase):
             if v.__class__ == list:
                 result_get_dict[k] = set(v)
         self.assertDictEqual(asset_dict, result_get_dict)
+
+
+    def test_asset_post_is_not_complete(self):
+        client = APIClient()
+        # Missing data_category_others but necessary as data_category include others,
+        # thus is_complete should return False
+        asset_dict = {
+            "data_subject": [
+                "students"
+            ],
+            "data_category": [
+                "others", "research"
+            ],
+            "risk_type": [
+                "operational", "reputational"
+            ],
+            "storage_format": [
+                "digital", "paper"
+            ],
+            "paper_storage_security": [
+                "locked_cabinet"
+            ],
+            "digital_storage_security": [
+                "acl"
+            ],
+            "name": "asset1",
+            "department": "department test",
+            "purpose": "don't know",
+            "owner": "amc203",
+            "private": True,
+            "personal_data": False,
+            "recipients_category": "no idea",
+            "recipients_outside_eea": False,
+            "recipients_outside_eea_who": "",
+            "retention": "<=1",
+            "retention_other": "",
+            "storage_location": "Who knows"
+        }
+
+        result_post = client.post('/assets/', asset_dict, format='json')
+        result_get = client.get(json.loads(result_post.content)['url'], format='json')
+        result_get_dict = json.loads(result_get.content)
+        del result_get_dict['url']
+        asset_dict['is_complete'] = False
+        asset_dict['data_category_others'] = None
+        for k, v in asset_dict.items():
+            if v.__class__ == list:
+                asset_dict[k] = set(v)
+        for k, v in result_get_dict.items():
+            if v.__class__ == list:
+                result_get_dict[k] = set(v)
+        self.assertDictEqual(asset_dict, result_get_dict)
