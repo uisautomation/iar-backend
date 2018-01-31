0aad82a49003980433fd012497de99dae28b7dd3
Rich Wareham <rjw57@cam.ac.uk>
2018-01-31 10:24:32 +0000

    assets: increate authentication test coverage
    The authentication tests did not test that an expired token results in
    the OAuth2 session being re-created. Modify the existing mock to mock
    the OAuth2Session object itself so that _get_session() is actually
    called.
    
    Add a test which mocks the cached session inside _get_session() to raise
    a TokenExpiredError which should result in the cached session being
    re-created and the token being validated successfully.

--- assets/tests/test_authentication.py
+++ assets/tests/test_authentication.py
@@ -11,6 +11,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from requests import Response
 from rest_framework.request import Request
+from oauthlib.oauth2 import TokenExpiredError
 
 from assets import authentication
 
@@ -34,7 +35,7 @@ class OAuth2Test(TestCase):
     def test_good_token(self):
         """A request with a good token is authenticated."""
         self.set_token(OAuth2Test.GOOD_TOKEN)
-        with self.patch_session():
+        with self.patch_oauth2_session():
             result = self.authenticate()
         self.assertIsNotNone(result)
         user, auth = result
@@ -47,7 +48,7 @@ class OAuth2Test(TestCase):
     def test_empty_subject(self):
         """A request with a good token but no subject creates no user."""
         self.set_token(OAuth2Test.NO_SUBJECT_TOKEN)
-        with self.patch_session():
+        with self.patch_oauth2_session():
             result = self.authenticate()
         self.assertIsNotNone(result)
         user, auth = result
@@ -57,23 +58,43 @@ class OAuth2Test(TestCase):
     def test_unknown_token(self):
         """A request with an unknown token is not authenticated."""
         self.set_token(OAuth2Test.UNKNOWN_TOKEN)
-        with self.patch_session():
+        with self.patch_oauth2_session():
             result = self.authenticate()
         self.assertIsNone(result)
 
     def test_past_token(self):
         """A request with an past token is not authenticated."""
         self.set_token(OAuth2Test.PAST_TOKEN)
-        with self.patch_session():
+        with self.patch_oauth2_session():
             result = self.authenticate()
         self.assertIsNone(result)
 
     def test_future_token(self):
         """A request with an future token is not authenticated."""
         self.set_token(OAuth2Test.FUTURE_TOKEN)
-        with self.patch_session():
+        with self.patch_oauth2_session():
             result = self.authenticate()
         self.assertIsNone(result)
+
+    def test_expired_token(self):
+        """A request with an expired token results in the session being re-created."""
+        self.set_token(OAuth2Test.GOOD_TOKEN)
+
+        def mocked_request(*args, **kwargs):
+            """Mocked request function for first call which raises TokenExpiredError."""
+            raise TokenExpiredError
+
+        # Mock the internal cached session so that mocked_request() is called and then, if all is
+        # working, a new session is obtained via the session patched by patch_oauth2_session.
+        cached_session_patch = mock.patch('assets.authentication._request.__session')
+
+        with cached_session_patch as cached_session, self.patch_oauth2_session():
+            cached_session.request.side_effect = mocked_request
+            result = self.authenticate()
+        self.assertIsNotNone(result)
+        user, auth = result
+        self.assertIsNotNone(user)
+        self.assertIsInstance(auth, dict)
 
     def authenticate(self):
         """Convenience method to authenticate self.request against self.auth."""
@@ -83,7 +104,7 @@ class OAuth2Test(TestCase):
         """Set the Bearer token for the request to *token*."""
         self.request.META['HTTP_AUTHORIZATION'] = 'Bearer ' + token
 
-    def patch_session(self):
+    def patch_oauth2_session(self):
         """Patch the internal request session used by the authenticator."""
         mock_request = mock.MagicMock()
 
@@ -119,10 +140,11 @@ class OAuth2Test(TestCase):
 
         mock_request.side_effect = side_effect
 
+        # Mock OAuth2Session to return our session mock
         mock_get_session = mock.MagicMock()
         mock_get_session.return_value.request = mock_request
 
-        return mock.patch('assets.authentication._get_session', mock_get_session)
+        return mock.patch('assets.authentication.OAuth2Session', mock_get_session)
 
 
 def _utc_now():
