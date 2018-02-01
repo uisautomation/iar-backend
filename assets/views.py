0e4c9984fa71cbb26f18520188b820e39a231de7
Abraham Martin <amc203@cam.ac.uk>
2018-02-01 20:48:34 +0000

    add filtering class for API

--- assets/views.py
+++ assets/views.py
@@ -3,14 +3,16 @@ Views for the assets application.
 
 """
 from django.utils.decorators import method_decorator
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets
-from rest_framework.pagination import CursorPagination
+from rest_framework import viewsets, generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .authentication import OAuth2TokenAuthentication
 from .models import Asset
 from .permissions import HasScopesPermission
 from .serializers import AssetSerializer
+
 
 REQUIRED_SCOPES = ['lookup:anonymous', 'assetregister']
 """
@@ -45,6 +47,14 @@ class AssetViewSet(viewsets.ModelViewSet):
     queryset = Asset.objects.all()
     serializer_class = AssetSerializer
     pagination_class = AssetCursorPagination
+    ordering = ('-created_at',)
+    filter_backends = (OrderingFilter, )
+    ordering_fields = \
+        ('id', 'name', 'department', 'purpose', 'owner', 'private', 'research',
+         'personal_data', 'data_subject', 'data_category', 'recipients_category',
+         'recipients_outside_eea', 'retention', 'risk_type', 'storage_location',
+         'storage_format', 'paper_storage_security', 'digital_storage_security',
+         'created_at', 'updated_at')
 
     authentication_classes = (OAuth2TokenAuthentication,)
     required_scopes = REQUIRED_SCOPES
@@ -54,3 +64,23 @@ class AssetViewSet(viewsets.ModelViewSet):
     # forward, we need to decide on a better permissions model based on the (client, scope, user)
     # triple.
     permission_classes = (HasScopesPermission,)
+
+@method_decorator(name='list', decorator=SCHEMA_DECORATOR)
+class AssetSearchViewSet(generics.ListAPIView):
+    """
+    API endpoint that allows assets to be searched.
+
+    """
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+    ordering = ('-created_at',)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filter_fields = search_fields = ordering_fields = \
+        ('id', 'name', 'department', 'purpose', 'owner', 'private', 'research',
+         'personal_data', 'data_subject', 'data_category', 'recipients_category',
+         'recipients_outside_eea', 'retention', 'risk_type', 'storage_location',
+         'storage_format', 'paper_storage_security', 'digital_storage_security',
+         'created_at', 'updated_at')
+
+    authentication_classes = (OAuth2TokenAuthentication,)
+    required_scopes = REQUIRED_SCOPES
