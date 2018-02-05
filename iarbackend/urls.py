01366b9edb0da77e715b1c2ab526c424d0736552
Abraham Martin <amc203@cam.ac.uk>
2018-02-05 14:40:11 +0000

    pep8 fix

--- iarbackend/urls.py
+++ iarbackend/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 import automationcommon.views
-from rest_framework.documentation import include_docs_urls
 
 # Django debug toolbar is only installed in developer builds
 try:
