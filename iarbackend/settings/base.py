9826d61f04ced8608a96b98a40d33f9fcfe739a2
Abraham Martin <amc203@cam.ac.uk>
2018-02-16 15:41:36 +0000

    correct name of the package

--- iarbackend/settings/base.py
+++ iarbackend/settings/base.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'assets.views.RequestUserMiddleware',
+    'automationcommon.middleware.RequestUserMiddleware',
 ]
 
 #: Root URL patterns
