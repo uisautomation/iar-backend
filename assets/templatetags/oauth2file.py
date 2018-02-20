6014e5021e377325fe3c21d6463b941ac375962f
Abraham Martin <amc203@cam.ac.uk>
2018-02-20 16:41:17 +0000

    use override_settings

--- assets/templatetags/oauth2file.py
+++ assets/templatetags/oauth2file.py
@@ -7,8 +7,9 @@ register = template.Library()
 
 
 @register.simple_tag
-def oauth2file(scheme, host):
-    oauth2fileurl = urlparse(static('iarbackend/oauth2-redirect.html'))
+def oauth2file(scheme, host, static_file):
+    """This custom tag always generates a FQDN for the specified static file"""
+    oauth2fileurl = urlparse(static(static_file))
     if oauth2fileurl.scheme == '' and oauth2fileurl.netloc == '':
-        return urljoin(str(scheme)+str(host), static('iarbackend/oauth2-redirect.html'))
-    return static('iarbackend/oauth2-redirect.html')
+        return urljoin(str(scheme)+"://"+str(host), static(static_file))
+    return static(static_file)
--- assets/tests/test_customtag.py
+++ assets/tests/test_customtag.py
@@ -1,16 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 
-class HasScopesTest(TestCase):
-    def test_static_file(self):
-        with self.settings(STATIC_URL='/static1/'):
-            response = self.client.get('/ui', follow=True)
-            import sys
-            sys.stdout.write(str(response.content))
-            self.assertContains(response,
-                                '"oauth2RedirectUrl": "/static1/iarbackend/oauth2-redirect.html"')
-        with self.settings(STATIC_URL='https://foobar/static2/'):
-            response = self.client.get('/ui', follow=True)
-            self.assertContains(
-                response,
-                '"oauth2RedirectUrl": "https://foobar/static2/iarbackend/oauth2-redirect.html"')
+class StaticFQDNTest(TestCase):
+
+    @override_settings(STATIC_URL='/static1/')
+    def test_static_normal_file(self):
+        response = self.client.get('/ui', follow=True)
+        self.assertContains(response,
+                            '"oauth2RedirectUrl": '
+                            '"http://testserver/static1/iarbackend/oauth2-redirect.html"')
+
+    @override_settings(STATIC_URL='https://foobar/static2/')
+    def test_static_s3_file(self):
+        response = self.client.get('/ui', follow=True)
+        self.assertContains(
+            response, '"oauth2RedirectUrl": '
+                      '"https://foobar/static2/iarbackend/oauth2-redirect.html"')
