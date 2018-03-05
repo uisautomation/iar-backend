8f8ad191075a216543853b3761ddefff9e6d7aba
Mike Bamford <mb2174@cam.ac.uk>
2018-03-05 10:14:56 +0000

    replaced current permission checking with DjangoModelPermissions, UserInInstitutionPermission, and OrPermission DRF permission classes.

--- assets/permissions.py
+++ assets/permissions.py
@@ -2,7 +2,13 @@
 OAuth2 token-based permissions for Django REST Framework views.
 
 """
+import logging
+
+from django.core.cache import cache
 from rest_framework import permissions
+
+
+LOG = logging.getLogger()
 
 
 class HasScopesPermission(permissions.BasePermission):
@@ -24,3 +30,97 @@ class HasScopesPermission(permissions.BasePermission):
         required_scopes = set(getattr(view, 'required_scopes', []))
 
         return required_scopes <= granted_scopes
+
+
+def validate_asset_user_institution(user=None, department=None):
+    """Validates that the user is member of the department that the asset belongs to
+    (asset_department). raises PermissionDenied if it doesn't, passes otherwise."""
+
+    if user is None or department is None:
+        return False
+
+    lookup_response = cache.get("{user.username}:lookup".format(user=user))
+    if lookup_response is None:
+        LOG.error('No cached lookup response for user %s', user.username)
+        return False
+
+    institutions = lookup_response.get('institutions')
+    if institutions is None:
+        LOG.error('No institutions in cached lookup response for user %s', user.username)
+        return False
+
+    institutions = map(lambda inst: inst['instid'],
+                       lookup_response.get('institutions', []))
+    if department not in institutions:
+        return False
+
+    return True
+
+
+class UserInInstitutionPermission(permissions.BasePermission):
+    """
+
+    """
+    def has_permission(self, request, view):
+        """
+        POST
+        :param request:
+        :param view:
+        :return:
+        """
+
+        # ????
+        if request.method != 'POST':
+            return True
+
+        assert 'department' in request.data
+
+        # Check permissions for write request
+        return validate_asset_user_institution(request.user, request.data['department'])
+
+    def has_object_permission(self, request, view, obj):
+        """
+        PUT PATCH DELETE
+        :param request:
+        :param view:
+        :param obj:
+        :return:
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        department = obj.department
+        if 'department' in request.data:
+            department = request.data['department']
+        return validate_asset_user_institution(request.user, department)
+
+def OrPermission(*args):
+    """
+    FIXME
+    :param args:
+    :return:
+    """
+    class OrPermission_:
+
+        def __init__(self):
+            self.permissions = [Permission() for Permission in args]
+            pass
+
+        def has_permission(self, request, view):
+            """
+            """
+            for permission in self.permissions:
+                if permission.has_permission(request, view):
+                    return True
+            return False
+
+        def has_object_permission(self, request, view, obj):
+            """
+            """
+            for permission in self.permissions:
+                if permission.has_object_permission(request, view, obj):
+                    return True
+            return False
+
+    return OrPermission_
+
